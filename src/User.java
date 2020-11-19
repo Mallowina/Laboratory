89c465f4a8a97b94911a26b821daf54a024dfc14
Mallowina <2003ksenia@mail.ru>
2020-11-19 21:47:02 +0300

    ver2

--- src/User.java
+++ src/User.java
@@ -12,14 +12,14 @@ public class User {
         FIO = fio;
         SNILS = snils;
 
-        out.println("\n\n -----------------------------------");
-        out.println("Приветствуем " + fio + "!");// c  входа
+        out.println("\n\n —---------------------------------");
+        out.println("Приветствуем " + fio + "!");// c входа
         System.out.println("++++++++++++++++++++++++++++++++++");
         System.out.println("1.Оставить заявку на исследование пробирки");
         System.out.println("2.Просмотреть существующие анализы");
         System.out.println("3.Выйти");
 
-        String Prob;
+        String Prob="";
 
         int action = 0;
         if (!scan.hasNextInt()) {
@@ -28,14 +28,16 @@ public class User {
         } else action = scan.nextInt();
 
         switch (action) {
-            case 1: {           //Добавление новой заявки
-                try(FileWriter writer = new FileWriter("application.txt", true))
+            case 1: {
+                try(FileWriter writer = new FileWriter("Application.txt", true))
                 {
-                    System.out.println("Введите номер пробирки");
-                    Prob = scan.next();
+                    System.out.println("Введите номер анализа");
+
+                    //Добавить номер анализа
+
                     writer.write(Prob + " " + SNILS);
                     writer.append('\n');
-                    System.out.println("!Вы успешно подали заявку!");
+                    System.out.println("Вы успешно подали заявку!");
                     writer.flush();
                     writer.close();
                     User(FIO, SNILS);
@@ -45,7 +47,30 @@ public class User {
                 }
                 User(FIO, SNILS);
             }
-            case 2: Data.List("analysis.txt"); User(FIO, SNILS);
+            case 2: Data.List("analysis.txt"); //User(FIO, SNILS)
+                try (Scanner Scan = new Scanner(new File("application.txt"))) {
+                    while (Scan.hasNextLine()) {
+                        String words[] = Scan.nextLine().split(" ");
+                        if (words[1].equals(snils)) {
+                            System.out.print("Пробирка "+ words[0]);
+                            System.out.println(" Не одобрена!");
+                        }
+                    }
+                } catch (FileNotFoundException fileNotFoundException) {
+                    fileNotFoundException.printStackTrace();
+                }
+                try (Scanner Scan = new Scanner(new File("Одобренные.txt"))) {
+                    while (Scan.hasNextLine()) {
+                        String worde[] = Scan.nextLine().split(" ");
+                        if (worde[1].equals(snils)) {
+                            System.out.print("Пробирка "+ worde[0]);
+                            System.out.println(" Одобрена!");
+                        }
+                    }
+                    User(FIO, SNILS);
+                } catch (FileNotFoundException fileNotFoundException) {
+                    fileNotFoundException.printStackTrace();
+                }
             case 3: Menu.Entry();
             default: {
                 out.println("Введи число соответсвующее списку\n");
