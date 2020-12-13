c8b8224742642c9d32d47aea55d60cba1b2b7814
Mallowina <2003ksenia@mail.ru>
2020-12-13 17:14:50 +0300

    add maven

--- src/main/java/Check.java
+++ src/main/java/Check.java
@@ -6,14 +6,13 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.regex.*;
 
-import static java.lang.System.out;
-
 public class Check {
     public static String checkN(String word) {  //Метод проверки таких данных как ФИО
         String regex = "[a-zA-Zа-яёА-ЯЁ]+";
 
         if (!word.matches(regex)) {
-            Alert alert = new Alert(Alert.AlertType.NONE, "Неверно введены данные ФИО.", ButtonType.OK);
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Input surname, name or patronymic again!", ButtonType.OK);
+            alert.setTitle("Input error");
             alert.showAndWait();
             return "no";
         }
@@ -24,7 +23,8 @@ public class Check {
         String regex = "\\d+";
 
         if (snils.length() != 11 || !snils.matches(regex)) {
-            Alert alert = new Alert(Alert.AlertType.NONE, "Неверно введены данные СНИЛС. Проверьте правильность ввода и количество символов", ButtonType.OK);
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Input again! Number of symbols isn't equal 11", ButtonType.OK);
+            alert.setTitle("Input error");
             alert.showAndWait();
             return "no";
         }
@@ -32,13 +32,15 @@ public class Check {
             while (Scan.hasNextLine()) {
                 String text[] = Scan.nextLine().split(" ");
                 if (snils.equals(text[0])) {
-                    Alert alert = new Alert(Alert.AlertType.NONE, "Такой СНИЛС уже используется", ButtonType.OK);
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION, "This INIPA already exist!", ButtonType.OK);
                     alert.showAndWait();
                     return "no";
                 }
             }
         } catch (FileNotFoundException e) {
-            System.out.printf("File [%s] is not found.\n");
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "File [%s] is not found!", ButtonType.OK);
+            alert.setTitle("File reading error");
+            alert.showAndWait();
         }
         return snils;
     }
@@ -50,13 +52,16 @@ public class Check {
             while (Scan.hasNextLine()) {
                 String text[] = Scan.nextLine().split(" ");
                 if (log.equals(text[0])) {
-                    Alert alert = new Alert(Alert.AlertType.NONE, "Такой логин уже существует", ButtonType.OK);
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION, "This login already exist!", ButtonType.OK);
+                    alert.setTitle("Login error");
                     alert.showAndWait();
                     return "no";
                 }
             }
         } catch (FileNotFoundException e) {
-            System.out.printf("File [%s] is not found.\n");
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "File [%s] is not found!", ButtonType.OK);
+            alert.setTitle("File reading error");
+            alert.showAndWait();
         }
         return log;
     }
@@ -68,7 +73,8 @@ public class Check {
         Matcher matcher = pattern.matcher(date);
 
         if (!matcher.matches()) {
-            Alert alert = new Alert(Alert.AlertType.NONE, "Неверно введена дата", ButtonType.OK);
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Input again! Check that the date format is correct ", ButtonType.OK);
+            alert.setTitle("Input error");
             alert.showAndWait();
             return "no";
         }
@@ -79,10 +85,11 @@ public class Check {
         Scanner scan = new Scanner(System.in);
 
         String regex = "^\\+?[78][-\\(]?\\d{3}\\)?-?\\d{3}-?\\d{2}-?\\d{2}$"; //допускает варианты:
-                                                                              //89103123167
+        //89103123167
         if (!tel.matches(regex)) {                                            //+7-910-221-22-22
-            Alert alert = new Alert(Alert.AlertType.NONE, "Неверно введен номер телефона", ButtonType.OK);
-            alert.showAndWait();                                              //+7(910)-221-22-22
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Input again!", ButtonType.OK);      //+7(910)-221-22-22
+            alert.setTitle("Input error");                                                               //ну и в таком духе
+            alert.showAndWait();                                            //+7(910)-221-22-22
             return "no";                                                      //ну и в таком духе
         }
         return tel;
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-
 public class Main extends Application{
     public static void main(String[] args) {
         Application.launch(args);
